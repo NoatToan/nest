cfa2539b2467a20f2d3009c33d1c82a6f851797b
toannt <toannt@hblab.vn>
2022-04-17 20:25:48 +0700

    project(swagger): config swagger

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCategoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCategoryDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  code: string;
+
+  @ApiProperty({ default: true })
+  is_active: boolean;
+}
